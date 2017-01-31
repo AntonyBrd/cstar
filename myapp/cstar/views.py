d288d1c427a2a8cb3d05837e5b273f60caa1e21d
brian82 <brian.blessou@gmail.com>
2017-01-31 21:50:33 +0100

    Create structure of the project.

---
+++ myapp/cstar/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'cstar/index.html', {})
+    
+def logs(request):
+    return render(request, 'cstar/logs.html', {})
+    
+def process(request):
+    return render(request, 'cstar/process.html', {})
+    
+def profils(request):
+    return render(request, 'cstar/profils.html', {})
+    
+
+

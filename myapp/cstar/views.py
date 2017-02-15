7195fb490e84c1e41fd7054ee3b2d6bf6a9bfcfe
AntonyBrd <antony.bernadou@gmail.com>
2017-02-15 13:22:19 +0100

    Python interaction

--- myapp/cstar/views.py
+++ myapp/cstar/views.py
@@ -1,27 +1,41 @@
 from django.shortcuts import render
+from django.contrib.staticfiles.storage import staticfiles_storage
 import json
+from django.contrib.staticfiles.templatetags.staticfiles import static
 #from cstar.processing_anomalies.simple_script import myFunc
 
-def myFunc(small_input):
-    return "{} : x value has been set to {}.".format(small_input, 2)
+
+def my_func(small_input):
+    data_sample = open('static/data/sample.json', 'r')
+    var = json.load(data_sample)
+    res = var.get("x")
+    var['newEntry'] = small_input
+    data_sample.close()
+    data_sample = open('static/data/sample.json', 'w')
+    json.dump(var, data_sample)
+    data_sample.close()
+    return "{} : x value has been set to {}.".format(small_input, res)
 
 
 def index(request):
     return render(request, 'cstar/index.html', {})
-    
+
+
 def logs(request):
     if request.method == 'GET':
-        result = myFunc("ok")
+        result = my_func("ok")
     else:
-        result = myFunc(request.POST['info_name'])
+        result = my_func(request.POST['info_name'])
     return render(request, 'cstar/logs.html',
                   {
                       'info': result,
                       'output': len(result)
                   })
-    
+
+
 def process(request):
     return render(request, 'cstar/process.html', {})
-    
+
+
 def profils(request):
     return render(request, 'cstar/profils.html', {})
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -102,9 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -121,5 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#print (">>>>>>>" + STATIC_ROOT)
 
 STATIC_URL = '/static/'
